8d8ae0b07013afd5a1d3e45a9a70e7c447979df9
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2024-08-16 21:00:26 +0300

    task1 v2

--- module_6_1.py
+++ module_6_1.py
@@ -1,54 +1,42 @@
-class  Animal:
-    alive = True  # живой
-    fed = False  # накормленный
+class Animal:
     def __init__(self, name):
         self.name = name
-
-
+        self.alive = True # живой
+        self.fed = False # накормленный
+    def eat(self, food):
+        if food.edible == True:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible == True:
-            print(f"{self.name} съел {food.name}")
-            fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            alive = False
-
+    def __init__(self, name):
+        super().__init__(name)
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible == True:
-            print(f"{self.name} съел {food.name}")
-            fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            alive = False
-
-#Flower, Fruit для Plant.
-class  Plant:
-    edible = False #съедобность
+    def __init__(self, name):
+        super().__init__(name)
+class Plant:
     def __init__(self, name):
         self.name = name
-
+        self.edible = False #съедобность
 class Flower(Plant):
-    edible = False
-
-
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = False
 class Fruit(Plant):
-    edible = True
-
-
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True #съедобность
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
-
 print(a1.name)
 print(p1.name)
-
 print(a1.alive)
 print(a2.fed)
 a1.eat(p1)
 a2.eat(p2)
 print(a1.alive)
 print(a2.fed)
-
